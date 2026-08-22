985aa5597f93abb92771c664e373a7fe333eb13b
Shi369 <Shi369@users.noreply.github.com>
2026-08-22 00:55:35 +0000

    deploy: 447ad394cc0178d6000646e6ebd6906e72c147d8

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01243",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "22",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "adb4b9a4083945a69a4685fab3370337"
+  }
+}
